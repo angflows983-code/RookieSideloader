b7f6c90226cc7beffe5608afa67a617e49e1047e
angflows983-code <angflows983@gmail.com>
2025-10-03 05:18:20 -0700

    Add files via upload

---
+++ Splash.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AndroidSideloader
+{
+    public partial class Splash : Form
+    {
+        public Splash()
+        {
+            InitializeComponent();
+        }
+
+        public void UpdateBackgroundImage(Image newImage)
+        {
+            this.BackgroundImage = newImage;
+        }
+    }
+}
